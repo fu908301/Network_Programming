007acda9c745edd22a2af5548db96682b9f37bb7
fu908301 <fu908301@gmail.com>
2016-06-14 03:12:49 +0800

    Three way handshake seqnum needs debug.

---
+++ step1_client.cpp
@@ -0,0 +1,132 @@
+#include <sys/types.h>
+#include <netdb.h>
+#include <fcntl.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+using namespace std;
+class TCP{
+  public:
+    TCP();
+    ~TCP();
+    void ini();
+    void get_seq_num();
+    void server(char *ip,char *C_PORT_NUM);
+    void three_way(char *_ip,char *_port);
+    void chartoint();
+    void inttochar();
+  private:
+    int port,myfd,seq_num,ack,n_seq_num;
+    char synack[10],syn[5],c_ack[5];
+    struct sockaddr_in myaddr,serveraddr;
+    struct hostent *hp;
+};
+TCP::TCP()
+{
+  port = 10260;
+}
+TCP::~TCP()
+{}
+void TCP::ini()
+{
+  myfd = socket(AF_INET,SOCK_STREAM,0);
+  bzero((char *)&myaddr, sizeof(myaddr));
+  myaddr.sin_family = AF_INET;
+  myaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+  myaddr.sin_port = htons(port);
+  if(bind(myfd,(struct sockaddr*)&myaddr,sizeof(myaddr)) < 0)
+  {
+    cout<<"Bind error."<<endl;
+    exit(1);
+  }
+}
+void TCP::get_seq_num()
+{
+  srand((int)time(NULL));
+  seq_num = rand() % 10000 + 1;
+}
+void TCP::server(char *ip,char *C_PORT_NUM)
+{
+  bzero(&serveraddr,sizeof(serveraddr));
+  serveraddr.sin_family = PF_INET;
+  hp = gethostbyname(ip);
+  if (hp == 0) 
+  {
+    cout<<"IP error"<<endl;
+    exit(1);
+  }
+  bcopy(hp->h_addr_list[0],(caddr_t)&serveraddr.sin_addr,hp->h_length);
+  int PORT_NUM = atoi(C_PORT_NUM);
+  serveraddr.sin_port = htons(PORT_NUM);
+  connect(myfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+}
+void TCP::three_way(char *_ip,char *_port)
+{
+  cout<<"=====Start the three-way handshake====="<<endl;
+  inttochar();
+  send(myfd,synack,sizeof(synack),0);
+  cout<<"Send a packet(SYN/ACK) to "<<_ip<<" : "<<_port<<endl;
+  int rec = recv(myfd,synack,sizeof(synack),0);
+  chartoint();
+  cout<<"Receive a packet(SYN/ACK) from "<<_ip<<" : "<<_port<<endl;
+  cout<<"       Receive a packet (seq_num = "<<n_seq_num<<", ack_num = "<<ack<<")"<<endl;
+  int temp;
+  temp = ack;
+  ack = n_seq_num++;
+  seq_num = ack;
+  inttochar();
+  send(myfd,synack,sizeof(synack),0);
+  cout<<"Send a packet(ACK) to "<<_ip<<" : "<<_port<<endl;
+  cout<<"=====Complete the three-way handshake====="<<endl;
+}
+void TCP::chartoint()
+{
+  for(int i = 0;i < 5;i++)
+    syn[i] = synack[i];
+  for(int i = 0;i < 5;i++)
+    c_ack[i] = synack[i+5];
+  n_seq_num = atoi(syn);
+  ack = atoi(c_ack);
+}
+void TCP::inttochar()
+{
+  int temp = 4;
+  for(int i = 0;i < 5;i++)
+  {
+      syn[i] = '0';
+      c_ack[i] = '0';
+    }
+  while(seq_num >= 1)
+  {
+      syn[temp] = seq_num % 10 + 48;
+      seq_num /= 10;
+      temp --;
+    }
+  temp = 4;
+  while(ack >= 1)
+  {
+      c_ack[temp] = ack % 10 + 48;
+      ack /= 10;
+      temp --;
+    }
+  for(int i = 0;i < 5; i++)
+    synack[i] = syn[i];
+  for(int i = 0;i < 5; i++)
+    synack[i+5] = c_ack[i];
+}
+int main(int argc, char**argv)
+{
+  TCP myTCP;
+  if(argc != 3)
+  {
+    cout<<"./client1 server_ip server_port"<<endl;
+    exit(1);
+  }
+  myTCP.get_seq_num();
+  myTCP.ini();
+  myTCP.server(argv[1],argv[2]);
+  myTCP.three_way(argv[1],argv[2]);
+}
---
+++ step1_sever.cpp
@@ -0,0 +1,193 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/ioctl.h>
+#include <netinet/in.h>
+#include <net/if.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#define BUFFER_SIZE 10240
+using namespace std;
+class TCP{
+  public:
+    TCP();
+    ~TCP();
+    void getip();
+    void print_parameters();
+    void create_data();
+    void get_seq_num();
+    void open_socket();
+    void check_bind();
+    void check_listen();
+    void go_listen();
+    void three_way();
+    void chartoint();
+    void inttochar();
+  private:
+    int ipfd,RTT,MSS,threshold,port,n_seq_num,listenfd,clientfd,ack,seq_num;
+    char _data[BUFFER_SIZE],synack[10],syn[5],c_ack[5];
+    struct sockaddr_in *addr,seraddr,cliaddr;
+    struct ifreq if_info;
+    char ip[INET_ADDRSTRLEN],cip[INET_ADDRSTRLEN];
+};
+TCP::TCP()
+{
+  RTT = 200;
+  MSS = 512;
+  threshold = 65535;
+  port = 10250;
+}
+TCP::~TCP()
+{}
+void TCP::getip()//get the server ip
+{
+  ipfd = socket(AF_INET, SOCK_DGRAM, 0);
+  strcpy(if_info.ifr_name, "eth0");
+  ioctl(ipfd, SIOCGIFADDR, &if_info);
+  addr = (struct sockaddr_in *)&if_info.ifr_addr;
+  inet_ntop(AF_INET,&addr->sin_addr,ip,sizeof(ip));
+}
+void TCP::print_parameters()
+{
+  cout<<"====Parameter===="<<endl;
+  cout<<"The RTT delay = "<<RTT<<" ms"<<endl;
+  cout<<"The threshold = "<<threshold<<" bytes"<<endl;
+  cout<<"The MSS = "<<MSS<<" bytes"<<endl;
+  cout<<"The buffer size = "<<BUFFER_SIZE<<" bytes"<<endl;
+  cout<<"Sever's IP is "<<ip<<endl;
+  cout<<"Sever is listening on port "<<port<<endl;
+  cout<<"==============="<<endl;
+}
+void TCP::create_data()//create the data randomly.
+{
+  int mark;
+  char char_table[62];
+  srand((int)time(NULL));
+  for(int i = 0;i < 26;i++)
+    char_table[i] = i + 65;
+  for(int i = 26;i < 52;i++)
+    char_table[i] = i + 71;
+  for(int i = 52;i < 62;i++)
+    char_table[i] = i - 4;
+  for(int i = 0;i < BUFFER_SIZE;i++)
+  {
+    mark = rand()%62;
+    _data[i] = char_table[mark];
+  }
+}
+void TCP::get_seq_num()
+{
+  seq_num = rand() % 10000 + 1;
+}
+void TCP::open_socket()
+{
+  listenfd = socket(PF_INET,SOCK_STREAM,0);
+  if(listenfd < 0)
+  {
+    cout<<"Create socket error."<<endl;
+    exit(1);
+  }
+  bzero(&seraddr,sizeof(seraddr));
+  seraddr.sin_family = AF_INET;
+  seraddr.sin_port = htons(port);
+  seraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+}
+void TCP::check_bind()
+{
+  if(bind(listenfd,(struct sockaddr*)&seraddr,sizeof(seraddr)) < 0)
+  {
+    cout<<"Bind error."<<endl;
+    exit(1);
+  }
+}
+void TCP::check_listen()
+{
+  if(listen(listenfd,10) < 0)
+  {
+    cout<<"Listen error."<<endl;
+    exit(1);
+  }
+}
+void TCP::go_listen()
+{
+  while(1)
+  {
+    socklen_t addrlen = sizeof(cliaddr);
+    cout<<"Listening for client..."<<endl;
+    clientfd = accept(listenfd,(struct sockaddr*)&cliaddr,&addrlen);
+    if(clientfd < 0)
+    {
+      cout<<"Accept error."<<endl;
+      exit(1);
+    }
+    three_way();
+  }
+}
+void TCP::three_way()
+{
+  cout<<"=====Start the three-way handshake====="<<endl;
+  int rec = recv(clientfd,synack,sizeof(synack),0);
+  chartoint();
+  inet_ntop(AF_INET, &cliaddr.sin_addr, cip, sizeof(cip));
+  cout<<"Receive a packet(SYN) from "<<cip<<" : "<<cliaddr.sin_port<<endl;
+  cout<<"       Receive a packet (seq_num = "<<n_seq_num<<", ack_num = "<<ack<<")"<<endl;
+  ack = n_seq_num + 1;
+  inttochar();
+  send(clientfd,synack,sizeof(synack),0);
+  cout<<"Send a packet(SYN/ACK) to "<<cip<<" : "<<cliaddr.sin_port<<endl;
+  rec = recv(clientfd,synack,sizeof(synack),0);
+  chartoint();
+  inet_ntop(AF_INET, &cliaddr.sin_addr,cip,sizeof(cip));
+  cout<<"Receive a packet(ACK) from "<<cip<<" : "<<cliaddr.sin_port<<endl;
+  cout<<"       Receive a packet (seq_num = "<<n_seq_num<<", ack_num = "<<ack<<")"<<endl;
+  cout<<"=====Complete the three-way handshake====="<<endl;
+}
+void TCP::chartoint()
+{
+  for(int i = 0;i < 5;i++)
+    syn[i] = synack[i];
+  for(int i = 0;i < 5;i++)
+    c_ack[i] = synack[i+5];
+  n_seq_num = atoi(syn);
+  ack = atoi(c_ack);
+}
+void TCP::inttochar()
+{
+  int temp = 4;
+  for(int i = 0;i < 5;i++)
+  {
+    syn[i] = '0';
+    c_ack[i] = '0';
+  }
+  while(seq_num >= 1)
+  {
+    syn[temp] = seq_num % 10 + 48;
+    seq_num /= 10;
+    temp --;
+  }
+  temp = 4;
+  while(ack >= 1)
+  {
+    c_ack[temp] = ack % 10 + 48;
+    ack /= 10;
+    temp --;
+  }
+  for(int i = 0;i < 5; i++)
+    synack[i] = syn[i];
+  for(int i = 0;i < 5; i++)
+    synack[i+5] = c_ack[i];
+}
+int main()
+{
+  TCP myTCP;
+  myTCP.getip();
+  myTCP.print_parameters();
+  myTCP.create_data();
+  myTCP.get_seq_num();
+  myTCP.open_socket();
+  myTCP.check_bind();
+  myTCP.check_listen();
+  myTCP.go_listen();
+}
