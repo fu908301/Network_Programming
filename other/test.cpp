f76948813ab66a347730a52670969acd02ccc110
fu908301 <fu908301@gmail.com>
2018-06-07 00:58:46 +0800

    Use a big buffer to store is better.

--- other/test.cpp
+++ other/test.cpp
@@ -4,42 +4,25 @@
 #include<cmath>
 using namespace std;
 int main(){
-	char buf[32468];
 	ifstream file1;
-	ifstream f1;
+	ifstream file2;
 	ofstream fileOut1;
 	file1.open("Video1.mp4", ios::in | ios::binary | ios::ate);
-	f1.open("Video1.mp4", ios::in | ios::binary);
+	if(!file1)
+		cout << "Open error 1" << endl;
 	fileOut1.open("out1.mp4", ios::out | ios::binary);
-	cout << "file size : " << file1.tellg() << endl;
-	int chunkNum = ceil((float)file1.tellg()/32468);	
-	cout << chunkNum << endl;
+	if(!fileOut1)
+		cout << "Open error 2" << endl;
 	int fileSize = file1.tellg();
-	/*streampos begin,end;
-	begin = file1.tellg();
-  	file1.seekg (0, ios::end);
-  	end = file1.tellg();
-	cout << "file size : " << (end - begin) << endl;*/
-	/*while(file1.peek()!=EOF){
-		file1.read(buf, sizeof(buf));
-		//cout << buf;
-		fileOut1 << buf;
-	}*/
-	
-	/*while(file1.read(buf.data(), size)){
-		cout << buf.data();
-		fileOut1 << buf.data();	
-	}*/
-
-	for(int i=0 ; i<chunkNum ; i++){
-		if(fileSize > buf)	fileSize -= buf;
-		
-		f1.read(buf, sizeof(buf));
-		cout << buf;
-		fileOut1.write(buf, sizeof(buf));
-	}
-	
+	char *buffer = new char[fileSize];
+	char ch;
+	int i = 0;
+	cout << "File size : " << fileSize << endl;
+	file1.seekg(0, ios::beg);
+	file1.read(buffer,fileSize);
+	fileOut1.write(buffer, fileSize);
 	file1.close();
-	f1.close();
 	fileOut1.close();
+	delete [] buffer;
+	return 0;
 }
